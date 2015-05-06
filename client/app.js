e9315193ecc50727461ba208d31a7d99b248aea2
Casie Lynch <cds.lynch@gmail.com>
2015-05-05 22:27:33 -0500

    initial commit, remove button doesnt work, style to come

---
+++ client/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('app', []);
+app.controller("IndexController", ['$scope', '$http', function($scope, $http){
+    $scope.cat = {};
+    $scope.cats = [];
+    var fetchCats = function(){
+        return $http.get('/cats').then(function(response){
+            if(response.status !== 200){
+                throw new Error('Failed to fetch cats from the API');
+            }
+            $scope.cat = {};
+            $scope.cats = response.data;
+            return response.data;
+        });
+    };
+    $scope.add = function(cat){
+        console.log(cat);
+        return $http.post('/add', cat).then(fetchCats);
+    };
+
+    $scope.remove = function(cat){
+        return $http.delete('/remove', cat);
+    }
+    fetchCats();
+}]);
